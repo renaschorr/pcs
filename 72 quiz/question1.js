8056e5844341859715c91f2bb8f65289d7cf38b6
renaschorr <137726460+renaschorr@users.noreply.github.com>
2023-09-10 21:09:48 +0300

    Add files via upload

---
+++ 72 quiz/question1.js
@@ -0,0 +1,18 @@
+
+Array.prototype.myMap = function(callback) {
+    const newArray = [];
+    for(let i = 0; i < this.length; i++) {
+      newArray.push(callback(this[i]));
+    }
+    return newArray;
+  }
+  
+  const originalArray = [3, 4, 6];
+  const newArray = originalArray.myMap(function(value) {
+    return value * 2;
+  });
+  
+  console.log("Original Array:", originalArray);
+  console.log("New Array:", newArray);
+  
+  
